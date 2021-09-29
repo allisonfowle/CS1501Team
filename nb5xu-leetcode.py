f87a374d029d32a8198b90b82f2c6ba0bc89b932
nadabencheikhh <nb5xu@virginia.edu>
2021-09-28 20:59:34 -0400

    Update nb5xu-leetcode.py

--- nb5xu-leetcode.py
+++ nb5xu-leetcode.py
@@ -1,1 +1,18 @@
-hi
+# Nada Bencheikh (nb5xu)
+# problem 217 contains duplicates 
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums)==0:
+            return False
+        else:
+            for i in range(len(nums)):
+                for j in range(len(nums)):
+
+                    if nums[i]==nums[j] and i!=j:
+                        return True
+            return False
